92c728a642a981c8d1ad7e98b7948bbbe60f7f70
lmouhandiz <156603278+archybox@users.noreply.github.com>
2024-05-26 19:47:00 +0100

    modify footer added info

--- js/jsindex.js
+++ js/jsindex.js
@@ -1,7 +1,8 @@
 
 let dict=[
     ['Create your professional CV','Choose a template, fill out the form and get your CV in minutes.'],
-    ['Créez votre CV professionnel','Choisissez un modèle, remplissez le formulaire et obtenez votre CV en quelques minutes.']
+    ['Créez votre CV professionnel','Choisissez un modèle, remplissez le formulaire et obtenez votre CV en quelques minutes.'],
+    ['أنشئ سيرتك الذاتية الاحترافية','اختر قالبًا، واملأ النموذج واحصل على سيرتك الذاتية في دقائق']
 ]
 
 
@@ -9,5 +10,6 @@ function switchlang(){
     let index = document.getElementById("selectlang").selectedIndex;
     h1id.innerText=dict[index][0];
     h2id.innerText=dict[index][1];
+    
 
 }
